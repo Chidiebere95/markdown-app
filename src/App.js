837672b9d4ec33b484fa42f5c9abf8c645af4412
Chidiebere Ezeokwelume <chidiezeokwelume@gmail.com>
2022-04-14 03:16:51 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
+
 function App() {
+const [markdown,setMarkDown]=useState('')
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea onChange={(e)=>setMarkDown(e.target.value)} className='markdown' value={markdown} placeholder="Write markdown here...">
+      </textarea>
+      <div className='text'>
+        <ReactMarkdown>
+           {markdown}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 }
